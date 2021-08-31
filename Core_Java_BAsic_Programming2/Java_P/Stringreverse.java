4506e159faeb06574fda82f2c08292a9395295f2
Sangeeta <Sangeeta@LAPTOP-GEDO5C2J>
2021-08-31 20:09:47 +0530

    2nd commit

--- Core_Java_BAsic_Programming2/Java_P/Stringreverse.java
+++ Core_Java_BAsic_Programming2/Java_P/Stringreverse.java
@@ -5,7 +5,7 @@ public class Stringreverse {
 	
 	public static void main(String[] args) {
         
-		// TODO Auto-generated method stub
+		// TODO34 Auto-generated method stub
 //        String str;
 //        Scanner in = new Scanner(System.in);
 //        System.out.println("Enter your String");
